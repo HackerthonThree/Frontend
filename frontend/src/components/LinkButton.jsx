04c44100e974555b0d42611a48f514dd4656e915
조현성 <joo9245@naver.com>
2021-11-10 21:36:06 +0900

    css조금바꿈

--- frontend/src/components/LinkButton.jsx
+++ frontend/src/components/LinkButton.jsx
@@ -5,7 +5,8 @@ import { useNavigate } from "react-router-dom";
 const style = {
   backgroundColor: "#f55e61",
 color: "#FFFFFF",
-width:"100px"
+width:"250px",
+height:"50px"
 };
 
 export default function LinkButton({ title, link }) {
@@ -14,7 +15,9 @@ export default function LinkButton({ title, link }) {
 
   return (
     <Button style={style} variant="contained" onClick={handleOnClick}>
+    <b>
       {title}
+      </b>
     </Button>
   );
 }
